e10d10446d3992ac4c3f729d9535848bf7f7dea2
MaxymSemenykhin <msem@solvve.com>
2023-03-31 00:37:38 +0300

    Code format udpate

--- config/db.js
+++ config/db.js
@@ -1,26 +1,28 @@
 const mongoose = require('mongoose');
-const { MONGO_URL } = require('./constants')
+const { MONGO_URL } = require('./constants');
 
 const connectDB = () => {
   try {
-    mongoose.connect(MONGO_URL,{
-      useNewUrlParser: true,
-          useCreateIndex: true,
-          useFindAndModify: false,
-          useUnifiedTopology: true
-    }).then(()=>{
-      console.log('MongoDB Connected...');
-    })
+    mongoose
+      .connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log('MongoDB Connected...');
+      });
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
-  process.on("SIGINT", async() => {
+  process.on('SIGINT', async () => {
     await mongoose.disconnect();
-    console.log("Received stop signal");
+    console.log('Received stop signal');
     process.exit();
   });
-  return mongoose.connection
-}
+  return mongoose.connection;
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const connectDB = require('./config/db');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const startServer = () => {
-    const PORT = process.env.SERVER_PORT || 5000;
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}
+  const PORT = process.env.SERVER_PORT || 5000;
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+};
 
-connectDB()
-    .on('error', console.log)
-    .once('open', startServer)
+connectDB().on('error', console.log).once('open', startServer);
 
 //Middleware
 app.use(express.json());
 app.use(express.static('.'));
-app.use(cors())
+app.use(cors());
 
 //Routes
 app.use('/', require('./routes/api/route'));
-
-
-
