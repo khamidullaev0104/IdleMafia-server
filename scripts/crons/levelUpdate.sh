7fdc663b7da043a15e398be54ef099b8ce52f26d
MaxymSemenykhin <msem@solvve.com>
2023-04-11 21:28:39 +0300

    Add bash cron job

---
+++ scripts/crons/levelUpdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source "output.sh"
+CRONS_DIR=../../crons
+
+print_title "Level date update"
+node "$CRONS_DIR/levelParse.js"
+
+print_info "search for new capos"
+node "$CRONS_DIR/capoParse.js"
+
+print_info "save capos name"
+node "$CRONS_DIR/capoSaveToLevelSchema.js"
