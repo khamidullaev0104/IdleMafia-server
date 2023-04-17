3fdd40cdb652599233ecf2fc4b2ec37122ec7c54
MaxymSemenykhin <msem@solvve.com>
2023-04-17 22:40:41 +0300

    Add 10 secs delay to cron level parse

--- crons/levelParse.js
+++ crons/levelParse.js
@@ -9,7 +9,7 @@ async function OnDbConnected() {
   if (process.env.DEBUG ?? false) console.log(`OnDbConnected`);
 
   await axiosPostToChannel('messages', process.env.CHANNEL_ID, 'level');
-  await new Promise((r) => setTimeout(r, 300));
+  await new Promise((r) => setTimeout(r, 10*1000));
 
   const BotfatherChannelId = await getChannelID(
     process.env.DISCORD_TOKEN,
